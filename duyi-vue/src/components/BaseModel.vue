52ddc236e02a5b1a1f8695a5a71be358259ee819
jieGit <2857705371@qq.com>
2020-09-30 10:15:35 +0800

    51

---
+++ duyi-vue/src/components/BaseModel.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+    data () {
+        return {
+            value: "shanshan"
+        }
+    },
+    render (h) {
+        var self = this;
+        return h('div', {}, [
+            h("input", {
+                attrs: {
+                    value: this.value,
+                },
+                on: {
+                    input (e) {
+                        self.value = e.target.value;
+                    }
+                }
+            }),
+            this.value,
+        ]);
+    }
+}
+</script>
