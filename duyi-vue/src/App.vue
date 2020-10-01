2b3f0532e3cd9bd95a49346b270192d8ae78f916
jieGit <2857705371@qq.com>
2020-10-01 15:49:33 +0800

    53

--- duyi-vue/src/App.vue
+++ duyi-vue/src/App.vue
@@ -1,99 +1,45 @@
+<template>
+    <div id="app">
+        app
+        <base-level :level="2" @click="handleClick">
+            
 
+            <!-- <template #default>
+                标题
+            </template>
+
+            <template #header>
+                标题
+            </template>
+
+            <div>div</div>
+            <p>p</p> -->
+
+
+
+            <template v-slot:header>
+                <p>first</p>
+            </template>
+            <p>scoped</p>
+        标题</base-level>
+    </div>
+</template>
 <script>
-// <template>
-//     <div id="app">
-//         app
-//     </div>
-// </template>
-// JSX == JS + XML(html)
-
-
-// <template>
-//     <div>
-//         <base-demo>
-//             <template #default>
-//                 <div>默认插槽</div>
-//             </template>
-//              <template #header>
-//                 <div>具名header插槽</div>
-//             </template>
-//         </base-demo>
-//     </div>
-// </template>
-
-// <template>
-//     <div>
-//         <base-demo v-slot="{ text }">
-//             {{ text }}
-//         </base-demo>
-//     </div>
-// </template>
-
-
-import BaseDemo from "./components/BaseDemo";
+import BaseLevel from "./components/BaseLevel";
 export default {
     name: "app",
-    data () {
+    provide () {
         return {
-            value: "hello world",
-            show: !true,
-            num: 1,
-            content: "shanshan"
+            name: "shanshan"
         }
     },
     components: {
-        BaseDemo,
+        BaseLevel,
     },
-    methods:{
-        vIf () {
-            if (this.num === 1){
-                return <div>1</div>
-            } else if (this.num === 2) {
-                return <span>2</span>
-            } else {
-                return <p>{ this.num }</p>
-            }
-        },
-        handleClick (num) {
-            // console.log("click")
-            console.log(num);
-        }
-    },
-    render () {
-        const scopedSlots = {
-            scopedSlots: {
-                default: props => <span>{ props.text }</span>,
-            }
-        }
-        return (
-            <h1>
-            { this.value }
-            <div domPropsInnerHTML="<a>href</a>"></div>
-            <div domPropsTextContent="<a>href</a>"></div>
-            <div v-show={this.show}>show</div>
-            { true && <div>if</div> }
-            { !true ? <div>div</div> : <span>span</span>}
-            { this.vIf() }
-            <ul>
-                { [1, 2, 3].map(item => <li ref="li" refInFor={true} key={item}>{item}</li>) }
-            </ul>
-            <div on-click={ () => { this.handleClick(1) } }>点我</div>
-            <base-demo navtiveOnclick={this.handleClick}></base-demo>
-            <div class={ ["a", "b"] } style={ {fontSize: "14px", color: "red"} }>v-bind</div>
-            <input ref="input" v-model={ this.content }/>
-            {this.content}
-            <base-demo>
-                <div slot="default">默认插槽</div>
-                <div slot="header">具名header插槽</div>
-            </base-demo>
-            <base-demo {...scopedSlots}>
-              
-            </base-demo>
-            </h1>
-        )
-    },
-    mounted () {
-        console.log(this.$refs);
+    methods: {
+        handleClick () {
+          console.log("xxxx");  
+        } 
     }
 }
 </script>
