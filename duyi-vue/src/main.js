aded841f64e555c31bbf85858e1b5560dd351466
jieGit <2857705371@qq.com>
2020-10-02 21:01:31 +0800

    59

--- duyi-vue/src/main.js
+++ duyi-vue/src/main.js
@@ -1,19 +1,15 @@
-import Vue from 'vue'
-import App from './App.vue'
-import "velocity-animate"
-import "animate.css"
+import Vue from 'vue';
+import App from './App.vue';
+import "./assets/reset.css";
+import router from "./router"
+
+
 
 Vue.config.productionTip = false
 
 
-//模板
-//el $mount
-//template el.outerHTML
-
-//完整版
-//运行时版 （少了编译器，体积小30%）
-
 new Vue({
   //template: "<p>haha deng is a p</p>",
   render: h => h(App),
-}).$mount('#app')
+  router,
+}).$mount('#app') 
